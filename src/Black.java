950c719de3105985f8de7d60025c91c756284b88
Silver-Lancer <12274006@bjtu.edu.com>
2017-04-14 09:50:36 +0800

    try to push my files to github.

---
+++ src/Black.java
@@ -0,0 +1,8 @@
+public class Black {
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String str = "  Java  class   ";
+		System.out.println("字符串原来的长度：" + str.length());
+		System.out.println("去掉空格后的长度：" + str.trim().length());
+	}
+}
